eb843c69b1f9be5acb56a744e70a4bfe8e70d6ef
Kai Steuernagel <kaisteuernagel@gmx.de>
2014-01-22 22:08:53 +0100

    removed logback. someone needs to find out how to use it correctly in an osgi environment.

--- optm/src/net/optm/ui/handler/ExitHandler.java
+++ optm/src/net/optm/ui/handler/ExitHandler.java
@@ -19,8 +19,6 @@ package net.optm.ui.handler;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.workbench.IWorkbench;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Handler for exit command. Exits the application.
@@ -30,12 +28,9 @@ import org.slf4j.LoggerFactory;
  */
 public class ExitHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(ExitHandler.class);
-
     @Execute
     public void execute(final IWorkbench workbench) {
         System.out.println("called exit command");
-        logger.debug("exit");
         workbench.close();
     }
 
